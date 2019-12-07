eea0b0d7d1ef9ad46647acb4d093025f60e3d220
mbsantiago <santiago.mbal@gmail.com>
2019-12-07 15:11:12 -0600

    Update to new visualizer structure

--- src/visualizer/index.js
+++ src/visualizer/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import VisualizerBase from '../VisualizerBase';
+import VisualizerBase from '@selia/visualizer';
 
 // import {AudioObj} from './Audio';
 import {canvasSetup} from './Graphics';
